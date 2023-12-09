f0c4212336b65c490643a3f605eed9cc1bd8b9e9
GeorgeeFlash <georgeeflash@gmail.com>
2023-12-09 06:27:29 +0100

    login component succesfully done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Routes, Route, useNavigate} from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import Login from './components/Login';
 import Home from './container/Home';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="login" element={<Login />} />
+      <Route path="login" element={
+        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>  
+          <Login />
+        </GoogleOAuthProvider>
+      } />
       <Route path="/*" element={<Home />} />
     </Routes>
   )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { FcGoogle } from 'react-icons/fc';
+// import { FcGoogle } from 'react-icons/fc';
 import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from 'jwt-decode';
+
+import { client } from '../client';
 import shareVideo from '../assets/share.mp4';
 import logo from '../assets/logowhite.png';
 
-const Login = () => {
 
-  // const credentialResponse = (response) => {
-    
-  // }
+const Login = () => {
+  const navigate = useNavigate();
+
+  const credentialResponse = (response) => {
+    let decoded = jwtDecode(response.credential)
+
+    localStorage.setItem('user', JSON.stringify(decoded))
+
+    const { name, sub, picture } = decoded;
+
+    const doc = {
+      _id: sub,
+      _type: 'user',
+      userName: name,
+      image: picture,
+    }
+
+    client.createIfNotExists(doc)
+      .then(() => {
+        navigate('/', { replace: true })
+      })
+  }
 
   return (
     <div className='flex justify-start items-center flex-col h-screen'>
@@ -23,18 +44,16 @@ const Login = () => {
           autoPlay
           className='w-full h-full object-cover'
         />
-        <div className="absolute flex flex-col justify-center item-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
           <div className="p-5">
             <img src={logo} width="130px" alt="logo" />
           </div>
-          <div className="shadow-2xl">
+          
+          <div className="shadow-2xl flex justify-center p-3 ouline-none cursor-pointer">
             <GoogleLogin 
-              onSuccess={credentialResponse => {
-                console.log(credentialResponse);
-              }}
-              onError={(credentialResponse) => {
-                console.log(credentialResponse);
-              }}
+              onSuccess={credentialResponse}
+              onError={credentialResponse}
             />
           </div>
         </div>
